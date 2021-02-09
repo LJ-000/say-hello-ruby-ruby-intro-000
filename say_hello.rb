c8411abc87bb77a7deff8840be86d5cbf450cd83
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-09 19:45:59 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,7 +4,7 @@ def say_hello (name, language)
 
     it 'accepts an argument of a name and prints out Hello with that Name' do
       expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-      say_hello("Kent Beck")
+      say_hello("Kent Beck, Laura")
     end
 
     it 'defaults to Ruby Programmer when no name is passed in' do
